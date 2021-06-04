ee27b3d5ba581a5e687d922c21811a16ccdfcc45
Sefer Sarı <sefersari90@gmail.com>
2021-06-04 03:31:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const env = process.env;
 
 const config = {
-    db: { /* don't expose password or any sensitive info, done only for demo */
+    db: {
         host: env.DB_HOST || 'localhost',
         user: env.DB_USER || 'root',
         password: env.DB_PASSWORD || '',
